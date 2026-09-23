063b4b954835f77535cd4a0de38d8db557850823
Karim Rmadan <karim-rmadan@gmx.de>
2026-09-23 14:18:33 +0200

    Harden Steckbrief

--- src/Steckbrief2.java
+++ src/Steckbrief2.java
@@ -4,18 +4,29 @@ public class Steckbrief2 {
     static void main() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Wie lautet dein Vorname? ");
-        String vorname = scanner.nextLine();
+       /* System.out.println("Wie lautet dein Vorname? ");
+        String vorname = scanner.nextLine().trim();
 
 
         System.out.print("Wie alt bist du? ");
-        int alter = Integer.parseInt(scanner.nextLine());
+        String eingabe = scanner.nextLine().trim();
+
+        int alter;
+
+        if (eingabe.isEmpty()) {
+            alter = 18;
+        } else {
+            alter = Integer.parseInt(eingabe);
+        }
 
         System.out.println("Wie groß bist du in Metern? ");
-        double groesseInMetern = Double.parseDouble(scanner.nextLine().replace(',', '.'));
+        double groesseInMetern = Double.parseDouble(scanner.nextLine().replace(',', '.').trim());
 
         System.out.println("Trinkst du Kaffee? (ja/nein)");
-        boolean istKaffeeTrinker = scanner.nextLine().equalsIgnoreCase("ja");
+        String kaffeeAntwort = scanner.nextLine();
+
+        boolean istKaffeeTrinker = kaffeeAntwort.equalsIgnoreCase("ja")||
+                kaffeeAntwort.equalsIgnoreCase("j");
 
 
 
@@ -24,5 +35,20 @@ public class Steckbrief2 {
         System.out.printf("\n\nName in Großbuchstaben: %s\nAnzahl der Zeichen: %d\nAnfangsbuchstabe: %c",vorname.toUpperCase(),vorname.length(),vorname.charAt(0));
 
         System.out.printf("\n\nKaffeetrinker: %s", istKaffeeTrinker ? "Ja" : "Nein");
+
+        */
+
+        System.out.println("Gebe deinen vollen Namen ein: ");
+        String vollerName = scanner.nextLine();
+
+        String vorname2 = vollerName.substring(0,vollerName.indexOf(" "));
+        String nachname2 = vollerName.substring(vollerName.indexOf(" " ) + 1, vollerName.length());
+
+        String rueckwaerts = new StringBuilder(vorname2).reverse().toString();
+
+        System.out.printf("Vorname: %s\nNachname: %s\nInitiale Vorname: %c\nInitiale Nachname: %c\nVorname rückwärts: %s",
+                vorname2,nachname2,vorname2.charAt(0),nachname2.charAt(0), rueckwaerts);
+        //wenn kein leerzeichen eingegeben wird,entsteht ein Exception Fehler
+
     }
 }
