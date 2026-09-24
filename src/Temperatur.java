474a1e966aa26e8bcd93de4f57ed9738e9164385
Karim Rmadan <karim-rmadan@gmx.de>
2026-09-24 08:59:32 +0200

    Aufgabe erledigt

--- src/Temperatur.java
+++ src/Temperatur.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Temperatur {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Grad Celsius? ");
+        int celsius = Integer.parseInt(scanner.nextLine().trim());
+
+       // int fahrenheit = celsius * 9 / 5 + 32;
+
+        double fahrenheit2 = (double) celsius * 9 / 5 + 32;
+
+        //System.out.println("Fahrenheit: " + fahrenheit + "\nFahrenheit 2: " + fahrenheit2);
+
+        System.out.printf("Fahrenheit: %.1f",fahrenheit2);
+
+
+    }
 }
