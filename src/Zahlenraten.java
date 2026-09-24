b600951a4018861026877919920656356c1489bb
Karim Rmadan <karim-rmadan@gmx.de>
2026-09-24 13:45:28 +0200

    Aufgabe Basis 1 von 2 erledigt

--- src/Zahlenraten.java
+++ src/Zahlenraten.java
@@ -1,2 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Zahlenraten {
+    static void main() {
+        Random zufallsZahl = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int geheim = zufallsZahl.nextInt(101);
+
+        //zähler für versuche angelegt bei 1, damit der erste versuch mitgezählt wird
+        int versuche = 1;
+        //boolean auf false gesetzt
+        boolean istErraten = false;
+
+        //while schleife bis boolean auf true springt
+        while (!istErraten) {
+            System.out.println("Gebe eine Zahl ein: ");
+            int zahl = scanner.nextInt();
+
+            if (zahl < geheim){
+                System.out.println("zu klein.");
+                versuche++;
+            }else if (zahl > geheim){
+                System.out.println("zu groß.");
+                versuche++;
+            }else {
+                System.out.println("Volltreffer!");
+                istErraten = true;
+                System.out.println("du hast " + versuche + " versuche benötigt!");
+            }
+
+
+        }
+    }
 }
