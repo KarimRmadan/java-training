ca2fbe8973164cf7c9b1855a784f7b65a35a80d7
Karim Rmadan <karim-rmadan@gmx.de>
2026-09-24 15:14:03 +0200

    ComputerRaet gelöst aber nicht optimal

--- src/ComputerRaet.java
+++ src/ComputerRaet.java
@@ -1,2 +1,38 @@
+import java.util.Scanner;
+
 public class ComputerRaet {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        int unten = 1;
+        int oben = 100;
+        int zaehler = 1;
+        boolean istErraten = false;
+
+        while (!istErraten) {
+            for (int i = 0; i < 7 ; i++) {
+                int tipp = (unten + oben) / 2;
+                System.out.println(tipp);
+                System.out.println("k,g oder r?");
+                String antwort = scanner.nextLine();
+
+                if (antwort.equals("k")) {
+                    oben = tipp - 1;
+
+                } else if (antwort.equals("g")) {
+                    unten = tipp + 1;
+
+                } else if (antwort.equals("r")) {
+                    System.out.println("Ziel Erreicht\n du hast " + (i + 1) + "Versuche gebraucht");
+                    istErraten = true;
+                    break;
+                }
+            } if (!istErraten) {
+                System.out.println("Du Schummelst!");
+                break;
+            }
+        }
+
+    }
 }
+
