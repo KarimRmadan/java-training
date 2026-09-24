ceef1c3131d3105cc2daf5a4078adf69a1ac9f82
Karim Rmadan <karim-rmadan@gmx.de>
2026-09-24 10:00:10 +0200

    Übung 3 Basis teil abgeschlossen

--- src/Kalender.java
+++ src/Kalender.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Kalender {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        //Eingabe einlesen
+        System.out.println("Gebe ein Jahr ein: ");
+        int jahr = scanner.nextInt();
+
+        //boolean anlegen
+        boolean istSchaltjahr;
+
+        //Prüfung ob Schaltjahr mit passenden Ausgaben
+        if (jahr % 4 == 0 && jahr % 100 != 0 || jahr % 400 == 0) {
+            istSchaltjahr = true;
+            System.out.println(jahr + " ist ein Schaltjahr.");
+        } else {
+            istSchaltjahr = false;
+            System.out.println(jahr + " ist kein Schaltjahr.");
+        }
+
+        //Eingabe einlesen
+        System.out.println("gebe eine Zahl für den Wochentag ein: ");
+        int tag = scanner.nextInt();
+
+        //Switch Case zum Ausgeben des Wochentages
+        switch (tag) {
+            case 1:
+                System.out.println("Montag");
+                break;
+            case 2:
+                System.out.println("Dienstag");
+                break;
+            case 3:
+                System.out.println("Mittwoch");
+                break;
+            case 4:
+                System.out.println("Donnerstag");
+                break;
+            case 5:
+                System.out.println("Freitag");
+                break;
+            case 6:
+                System.out.println("Samstag");
+                break;
+            case 7:
+                System.out.println("Sonntag");
+                break;
+            default:
+                System.out.println("Ist kein Wochentag!");
+        }
+    }
 }
--- src/Temperatur.java
+++ src/Temperatur.java
@@ -4,15 +4,16 @@ public class Temperatur {
     static void main() {
         Scanner scanner = new Scanner(System.in);
 
+        //Eingabe einlesen
         System.out.println("Grad Celsius? ");
         int celsius = Integer.parseInt(scanner.nextLine().trim());
 
+        //berechnungen
        // int fahrenheit = celsius * 9 / 5 + 32;
-
         double fahrenheit2 = (double) celsius * 9 / 5 + 32;
 
+        //Ausgaben
         //System.out.println("Fahrenheit: " + fahrenheit + "\nFahrenheit 2: " + fahrenheit2);
-
         System.out.printf("Fahrenheit: %.1f",fahrenheit2);
 
 
